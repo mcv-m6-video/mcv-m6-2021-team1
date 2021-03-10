785fcb0d90bcac74c176bfd8e72eed6677079446
jchavescoelho <jchavescoelho@gmail.com>
2021-03-10 10:38:45 +0100

    WIP: Kind of a rework of what was before. Now main and utils are the important files, AP is on utils get_AP(...)

---
+++ week1/main.py
@@ -0,0 +1,81 @@
+""" 
+    MOTChallenge format [frame, ID, left, top, width, height, 1, -1, -1, -1].
+    <frame>, <id>, <bb_left>, <bb_top>, <bb_width>, <bb_height>, <conf>, <x>, <y>, <z> 
+"""
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import utils
+
+from matplotlib.pyplot import Figure
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+#PATHS
+RUN_NAME = 'mask_rcnn'
+annotations_file = '../../data/ai_challenge_s03_c010-full_annotation.xml'
+VIDEO_PATH = '../../data/AICity_data/train/S03/c010/vdo.avi'
+GT_PATH = '../../data/AICity_data/train/S03/c010/gt/gt.txt'
+DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_mask_rcnn.txt'
+DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_mask_rcnn.txt'
+DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_mask_rcnn.txt'
+
+
+def main():
+
+    if not os.path.exists(f'runs/{RUN_NAME}/'):
+        os.mkdir(f'runs/{RUN_NAME}/')
+
+    gt_all_rects = utils.parse_xml_rects(annotations_file)
+
+    tol_dropout = 0.1
+    std_pos = 0.2
+    std_size = 0.5
+    std_ar = 0.3
+    det_all_rects = utils.generate_noisy_bboxes(gt_all_rects, tol_dropout, std_pos, std_size, std_ar)
+
+    # Render video
+    cap = cv2.VideoCapture(VIDEO_PATH)
+    miou_over_time = []
+    frame_cont = 0
+    ret, frame = cap.read()
+    while(ret):
+    
+        gt_rects = gt_all_rects.get(frame_cont, None)
+        det_rects = det_all_rects.get(frame_cont, None)
+
+        if gt_rects:
+            for r in gt_rects:
+                frame = cv2.rectangle(frame, (int(r[0]), int(r[1])),  (int(r[2]), int(r[3])), (0, 255, 0))
+
+        if det_rects:
+            for r in det_rects:
+                frame = cv2.rectangle(frame, (int(r[0]), int(r[1])),  (int(r[2]), int(r[3])), (0, 0, 255))
+
+        if gt_rects and det_rects:
+            miou = utils.get_frame_iou(gt_rects, det_rects)
+            miou_over_time.append(miou)
+            utils.get_AP(gt_rects, det_rects)
+            print('Frame', frame_cont, 'iou:', miou)
+        else:
+            miou_over_time.append(miou)
+        
+        plt.plot(miou_over_time)
+        plt.xlim([0, 2140])
+        display_frame = cv2.resize(frame, tuple(np.int0(0.5*np.array(frame.shape[:2][::-1]))))
+        cv2.imshow('frame',display_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        plt.savefig(f'runs/{RUN_NAME}/iou_plt_{frame_cont}.png')
+        cv2.imread(f'runs/{RUN_NAME}/frame_{frame_cont}.png')
+
+        ret, frame = cap.read()
+        frame_cont += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+main()
---
+++ week1/utils.py
@@ -0,0 +1,202 @@
+import random
+import numpy as np
+import xml.etree.ElementTree as ET
+
+from sklearn.metrics import average_precision_score
+from matplotlib import pyplot as plt
+
+
+def parse_xml_rects(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+    frame_dict = {}
+    for child in root:
+        if child.tag=='track' and child.attrib['label']=='car':
+            for x in child:
+                d = x.attrib
+                if int(d['frame']) not in frame_dict:
+                    frame_dict[int(d['frame'])] = []
+                frame_dict[int(d['frame'])].append([float(d['xtl']), float(d['ytl']), 
+                    float(d['xbr']), float(d['ybr'])])
+    return frame_dict
+
+
+def get_vertex_from_cwh(center, width, height):
+    xtl = center[0] - width//2
+    xbr = center[0] + width//2
+    ytl = center[1] - height//2
+    ybr = center[1] + height//2
+
+    return xtl, ytl, xbr, ybr
+
+
+def generate_noisy_bboxes(frame_dict, tol_dropout, std_pos, std_size, std_ar):
+
+    noisy_dct = {}
+
+    for frame, bbs in frame_dict.items():
+        for bb in bbs:
+            
+            if random.random() > tol_dropout:     #random dropout
+                
+                xtl, ytl, xbr, ybr = bb
+
+                w = abs(xtl - xbr)
+                h = abs(ytl - ybr)
+
+                #position noise
+                center = np.array([w/2+xtl, h/2+ytl])
+                center += np.random.normal(0, std_pos*w, 2)
+                
+                #size noise
+                scale_f = np.random.normal(1, std_size)
+                h *= scale_f
+                w *= scale_f
+
+                #Aspect ratio noise
+                h *= np.random.normal(1, std_ar)
+                w *= np.random.normal(1, std_ar)
+
+
+                if frame not in noisy_dct:
+                    noisy_dct[frame] = []
+                noisy_dct[frame].append(get_vertex_from_cwh(center, w, h))
+
+    return noisy_dct
+
+
+def get_rect_iou(a, b):
+    x11, y11, x12, y12 = a
+    x21, y21, x22, y22 = b
+
+    xA = max(x11,x21)
+    yA = max(y11,y21)
+    xB = min(x12,x22)
+    yB = min(y12,y22)
+     
+    # respective area of ​​the two boxes
+    boxAArea=(x12-x11)*(y12-y11)
+    boxBArea=(x22-x21)*(y22-y21)
+     
+     # overlap area
+    interArea=max(xB-xA,0)*max(yB-yA,0)
+     
+     # IOU
+    return interArea/(boxAArea+boxBArea-interArea)
+
+
+# def parse_aicity_rects(path):
+# Not sure why this function is twice
+#     ret_dict = {}
+#     dtf = pd.read_csv(path, delimiter=',', names=COL_NAMES)
+
+#     for i, row in dtf.iterrows():
+#         if row.frame not in ret_dict:
+#             ret_dict[row.frame] = []
+#         ret_dict[row.frame].append([row.bb_left, row.bb_top, row.bb_left+row.bb_width, row.bb_top+row.bb_height])
+
+#     return ret_dict
+
+def get_frame_iou(gt_rects, det_rects):
+    list_iou = []
+
+    for gt in gt_rects:
+        max_iou = 0
+        for det in det_rects:
+            iou = get_rect_iou(det, gt)
+            if iou > max_iou:
+                max_iou = iou
+        list_iou.append(max_iou)
+
+    return np.mean(list_iou)
+
+
+def get_AP(gt_rects, det_rects):
+    """Our shot at Average precision"""
+
+    correct = np.zeros(len(det_rects))
+    conf = np.ones(len(det_rects)) # simulation of confidence levels
+
+    previous_detections = set()
+    for i, det in enumerate(det_rects):
+        for j, gt in enumerate(gt_rects):
+            iou = get_rect_iou(det, gt)
+
+            if iou > 0.5 and j not in previous_detections:
+                correct[i] = 1
+                previous_detections.add(j)
+
+    print('Scikit learn AP', average_precision_score(correct, conf, 'samples'))
+
+    ac_correct = np.array([np.sum(correct[:i]) for i in range(1, len(det_rects))])
+
+    precision = ac_correct / (np.array(range(len(ac_correct)))+1)
+    recall = ac_correct / len(gt_rects)
+
+    # Values at infinity
+    precision = np.hstack([precision, 0])
+    recall = np.hstack([recall, 1])
+
+    # Interpolate
+    interp_precision = []
+    val = precision[-1]
+    for i in range(len(precision) - 1, -1, -1):
+        if precision[i-1] > precision[i] :
+            val = precision[i]
+        interp_precision.insert(0, val)
+
+    sample = []
+    sample_idx = np.linspace(0, 1, 11)
+    for value in sample_idx:
+        idx = min(range(len(sample_idx)), key=lambda i: abs(sample_idx[i]-value))
+        try:
+            sample.append(max(interp_precision[idx:]))
+        except ValueError:
+            sample.append(0)
+    
+    
+    i = len(recall) -2
+    # interpolation...
+    precision2=precision.copy()
+    while i>=0:
+        if precision[i+1]>precision[i]:
+            precision[i]=precision[i+1]
+        i=i-1
+
+    # plotting...
+    # fig, ax = plt.subplots()
+    # for i in range(recall.shape[0]-1):
+    #     ax.plot((recall[i],recall[i]),(precision[i],precision[i+1]),'k-',label='',color='red') #vertical
+    #     ax.plot((recall[i],recall[i+1]),(precision[i+1],precision[i+1]),'k-',label='',color='red') #horizontal
+    # plt.plot(precision, recall)
+    # plt.scatter(np.linspace(0, 1, 11), sample)
+
+    # plt.xlim([0, 1])
+    # plt.ylim([0, 1])
+    # ax.set_xlabel("Recall")
+    # ax.set_ylabel("Precision")
+    # # plt.savefig('fig.jpg')
+    # plt.show()
+
+
+def rectangle_mot_format(img, left, top, width, height, color, thickness=None, conf=1, id=1):
+    left = int(left)
+    top = int(top)
+    width = int(width)
+    height = int(height)
+
+    return cv2.rectangle(img, (left, top), (left+width, top+height), color, thickness)
+  
+
+def parse_aicity_rects(path):
+    COL_NAMES = ['frame', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf', 'x', 'y', 'z']
+
+    ret_dict = {}
+    dtf = pd.read_csv(path, delimiter=',', names=COL_NAMES)
+
+    for i, row in dtf.iterrows():
+        if row.frame not in ret_dict:
+            ret_dict[row.frame] = []
+        ret_dict[row.frame].append([row.bb_left, row.bb_top, row.bb_left+row.bb_width, row.bb_top+row.bb_height])
+
+    return ret_dict
