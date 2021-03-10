78a71ff8a5b2a461b327d648a34567405688a7af
jchavescoelho <jchavescoelho@gmail.com>
2021-03-10 11:52:19 +0100

    Fixed some bugs on file generation

--- week1/main.py
+++ week1/main.py
@@ -5,7 +5,6 @@
 import os
 import cv2
 import numpy as np
-import pandas as pd
 import utils
 
 from matplotlib.pyplot import Figure
@@ -13,14 +12,23 @@ from matplotlib import pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 #PATHS
-RUN_NAME = 'mask_rcnn'
 annotations_file = '../../data/ai_challenge_s03_c010-full_annotation.xml'
 VIDEO_PATH = '../../data/AICity_data/train/S03/c010/vdo.avi'
 GT_PATH = '../../data/AICity_data/train/S03/c010/gt/gt.txt'
-DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_mask_rcnn.txt'
-DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_mask_rcnn.txt'
+
+RUN_NAME = 'rcnn'
 DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_mask_rcnn.txt'
 
+RUN_NAME = 'ssd'
+DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_ssd512.txt'
+
+RUN_NAME = 'yolo'
+DET_PATH = '../../data/AICity_data/train/S03/c010/det/det_yolo3.txt'
+
+# RUN_NAME = 'noisy'
+noisy = False
+show = False
+save=False
 
 def main():
 
@@ -30,10 +38,14 @@ def main():
     gt_all_rects = utils.parse_xml_rects(annotations_file)
 
     tol_dropout = 0.1
-    std_pos = 0.2
-    std_size = 0.5
+    std_pos = 0.1
+    std_size = 0.15
     std_ar = 0.3
-    det_all_rects = utils.generate_noisy_bboxes(gt_all_rects, tol_dropout, std_pos, std_size, std_ar)
+
+    if noisy:
+        det_all_rects = utils.generate_noisy_bboxes(gt_all_rects, tol_dropout, std_pos, std_size, std_ar)
+    else:
+        det_all_rects = utils.parse_aicity_rects(DET_PATH)
 
     # Render video
     cap = cv2.VideoCapture(VIDEO_PATH)
@@ -60,25 +72,58 @@ def main():
             miou_over_time.append(miou)
             ap = utils.get_AP(gt_rects, det_rects)
             map_over_time.append(ap)
-            print('Frame', frame_cont, 'iou:', miou, 'ap:', ap)
-        else:
-            miou_over_time.append(miou)
+            # print('Frame', frame_cont, 'iou:', miou, 'ap:', ap)
         
-        plt.plot(miou_over_time)
-        plt.xlim([0, 2140])
         display_frame = cv2.resize(frame, tuple(np.int0(0.5*np.array(frame.shape[:2][::-1]))))
-        cv2.imshow('frame',display_frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if show:        
+            cv2.imshow('frame',display_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        if save:
+            plt.figure()
+            plt.plot(miou_over_time)
+            plt.xlim([0, 2140])
+            plt.ylim([0, 1])
+            plt.savefig(f'runs/{RUN_NAME}/iou_plt_{frame_cont}.png')
 
-        plt.savefig(f'runs/{RUN_NAME}/iou_plt_{frame_cont}.png')
-        cv2.imread(f'runs/{RUN_NAME}/frame_{frame_cont}.png')
+            plt.figure()
+            plt.plot(map_over_time)
+            plt.xlim([0, 2140])
+            plt.ylim([0, 1])
+            plt.savefig(f'runs/{RUN_NAME}/map_plt_{frame_cont}.png')
+
+            cv2.imwrite(f'runs/{RUN_NAME}/frame_{frame_cont}.png')
+
 
         ret, frame = cap.read()
         frame_cont += 1
 
     cap.release()
     cv2.destroyAllWindows()
+
+    mAP = np.mean(map_over_time)
+    mIOU = np.mean(miou_over_time)
+
+    print(f'Mean statistics for {RUN_NAME}:\nmAP: {mAP}\nmIOU: {mIOU}')
+
+    plt.figure()
+    plt.plot(miou_over_time)
+    plt.xlim([0, 2140])
+    plt.ylim([0, 1])
+    plt.xlabel('# Frame')
+    plt.ylabel('mean IoU')
+    plt.title(f'Mean IoU over time for {RUN_NAME} data')
+    plt.savefig(f'runs/{RUN_NAME}_iou_plt_final.png')
+
+    plt.figure()
+    plt.plot(map_over_time)
+    plt.xlim([0, 2140])
+    plt.ylim([0, 1])
+    plt.xlabel('# Frame')
+    plt.ylabel('mean AP')
+    plt.title('Mean Average Precision over time for {RUN_NAME} data')
+    plt.savefig(f'runs/{RUN_NAME}_map_plt_final.png')
 
 main()
