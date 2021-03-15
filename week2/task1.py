4c2641d6246204584b2ac16c08f998acc4078676
barquerogerman <barquerogerman@gmail.com>
2021-03-15 19:57:53 +0100

    [W2] Big refactoring. Documentation added. Task 2.1 v0 implemented.

--- week2/task1.py
+++ week2/task1.py
@@ -3,72 +3,72 @@ import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
+import imageio
+import argparse
+from models import GaussianModel, AdaptiveGaussianModel
 
 # 2141 frames in total
 TOTAL_FRAMES = 2141
-PERCENTAGE = 0.05
-ALPHA = 11
+#PERCENTAGE = 0.25
+#ALPHA = 11
+#P = 0.001
+
 VIDEO_PATH = "../../AICity_data/train/S03/c010/vdo.avi"
 
-class GaussianModel:
+def post_processing(foreground):
+    return foreground
 
-    def __init__(self, frames, width, height, alpha):
-        self.alpha = alpha
-        self.images = np.zeros((width, height, frames))
-        self.mean = None
-        self.std = None
-        self.frames = frames
-        self.width = width
-        self.height = height
-        self.counter = 0
 
-    def __compute_parameters(self):
-        print("Modeling the background...")
-        self.mean = self.images.mean(axis=-1)
-        self.std = self.images.std(axis=-1)
-        #cv2.imwrite("mean.png", self.mean*255)
-        print("Done!")
+def main(args):
 
-    def get_foreground(self, I):
-        if self.mean is None or self.std is None:
-            self.__compute_parameters()
-        
-        return (I - self.mean >= self.alpha * (self.std + 2)).astype(np.uint8)*255
-
-    def add(self, I):
-        self.images[:,:,self.counter % self.frames] = I
-        self.counter += 1
-
-def main():
     if not os.path.exists(VIDEO_PATH):
         print("Video does not exist.")
         return
 
-    cap = cv2.VideoCapture(VIDEO_PATH)
+    model_frames = int(args.percentage * TOTAL_FRAMES)
 
-    flag, frame = cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if args.model == "gm":
+        model = GaussianModel(VIDEO_PATH, model_frames, ALPHA, f"{args.percentage}")
+        MODEL_NAME = "GaussianModel"
+    elif args.model == "agm":
+        model = AdaptiveGaussianModel(VIDEO_PATH, model_frames, args.alpha, args.p, f"{args.percentage}")
+        MODEL_NAME = "AdaptiveGaussianModel"
+    else:
+        raise Exception
 
-    model_frames = int(PERCENTAGE * TOTAL_FRAMES)
-    print(frame.shape)
-    model = GaussianModel(model_frames, frame.shape[0], frame.shape[1], ALPHA)
+    model.model_background()
+
+    results_path = f"results/{MODEL_NAME}/{args.alpha}_{args.percentage}"
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+    writer = imageio.get_writer(f"{results_path}/video.mp4", fps=25)
+
+    foreground = model.compute_next_foreground()
+    writer.append_data(foreground)
     counter = 0
-    while frame is not None:
-        flag, frame = cap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        if counter < PERCENTAGE * TOTAL_FRAMES:
-            model.add(frame)
-            
-        foreground = model.get_foreground(frame)
-        #cv2.imwrite(f"results/fg_{counter}.png", foreground)
-            
-        #if counter > PERCENTAGE * TOTAL_FRAMES + 50:
-        #    break
-
+    while foreground is not None:
+        foreground = post_processing(foreground)
+        #cv2.imwrite(f"results/{args.alpha}_{args.percentage}/fg_{counter}.png", foreground)
+        writer.append_data(foreground)
         counter += 1
-    print(counter)
 
-    cap.release()
+        foreground = model.compute_next_foreground()
+        if counter % 500 == 0:
+            print(f"[{counter}]")
 
-main()
+        if args.max != -1 and counter >= args.max:
+            break
+
+    print(f"DONE! {counter} frames processed")
+    writer.close()
+
+
+parser = argparse.ArgumentParser(description='Extract foreground from video.')
+parser.add_argument('-m', '--model', type=str, default='gm', choices=["gm", "agm"], help="model used for background modeling")
+parser.add_argument('-M', '--max', type=int, default=-1, help="max of frames for which infer foreground")
+parser.add_argument('-perc', '--percentage', type=float, default=0.25, help="percentage of video to use for background modeling")
+parser.add_argument('-a', '--alpha', type=float, default=11, help="alpha value")
+parser.add_argument('-p', '--p', type=float, default=0.001, help="[AdaptiveGaussianModel] parameter controlling the inclusion of new information to model")
+args = parser.parse_args()
+
+main(args)
