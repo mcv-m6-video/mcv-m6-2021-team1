145458640727b1794eada5786b1baf2ac84c699c
jchavescoelho <jchavescoelho@gmail.com>
2021-03-16 23:05:57 +0100

    Computing mAP without static objects. Added display option to command line

--- week2/main.py
+++ week2/main.py
@@ -19,6 +19,25 @@ TOTAL_FRAMES = 2141
 VIDEO_PATH = "../../data/AICity_data/train/S03/c010/vdo.avi"
 GT_RECTS_PATH = "../../data/ai_challenge_s03_c010-full_annotation.xml"
 
+def imshow_rects(im, rect_list, name):
+    im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
+    for det in rect_list:
+        rects = det['rects']
+        color = det['color']
+
+        for obj in rects:
+            r = obj['bbox']
+            im = cv2.rectangle(im, (int(r[0]), int(r[1])), (int(r[2]), int(r[3])), color, 3)    
+    display_resized(name, im)
+
+def NMS(rects):
+    """
+    Performs non maximum suppression.
+    Input is a list of rects in the format [{'bbox': [x1,y1,x2,y2], 'conf': 1}, ...]
+    """
+    idx = utils.non_max_suppression_fast(np.array([r['bbox'] for r in rects]), 0.5)
+    return [r for i, r in enumerate(rects) if i in idx]
+
 def display_resized(name, im, sf=0.5):
     im = cv2.resize(im, tuple(np.int0(sf*np.array(im.shape[:2][::-1]))))
     cv2.imshow(name, im)
@@ -88,10 +107,11 @@ def post_processing(foreground):
     # Get contours
     out_im, recs = analyse_contours(foreground)
 
-    # TODO: NMS
+    # NMS
+    recs = NMS(recs)
     
     # display_resized('after', out_im)
-    return out_im, recs
+    return out_im, recs # TODO: return out im
 
 
 def main(args):
@@ -120,18 +140,32 @@ def main(args):
         os.makedirs(results_path)
     writer = imageio.get_writer(f"{results_path}/video.mp4", fps=25)
 
-    foreground = model.compute_next_foreground()
+    foreground, I = model.compute_next_foreground()
     writer.append_data(foreground)
-    counter = 0
+    counter = int(TOTAL_FRAMES*args.perc)
     det_rects = {}
+    gt_rects = utils.parse_xml_rects(GT_RECTS_PATH, True) 
+    gt_rects_detformat = {f: [{'bbox': r, 'conf':1} for r in v] for f, v in gt_rects.items()}
+
     while foreground is not None:
         foreground, recs = post_processing(foreground)
+
         det_rects[f'f_{counter}'] = recs
+
+        if args.display:
+            imshow_rects(I, [{'rects': recs, 'color': (0,0,255)}, 
+                {'rects': gt_rects_detformat.get(f'f_{counter}', []), 'color': (0,255,0)}], 'result')
+
         #cv2.imwrite(f"results/{args.alpha}_{args.percentage}/fg_{counter}.png", foreground)
         writer.append_data(foreground)
         counter += 1
 
-        foreground = model.compute_next_foreground()
+        ret = model.compute_next_foreground()
+        if ret:
+            foreground, I = ret
+        else:
+            foreground = None
+
         if counter % 100 == 0:
             print(f"{counter} frames processed...")
 
@@ -142,7 +176,6 @@ def main(args):
     writer.close()
     print(f"Saved to '{results_path}'")
 
-    gt_rects = utils.parse_xml_rects(GT_RECTS_PATH)
     mAP = utils.get_AP(gt_rects, det_rects)
     print('mAP:', mAP)
 
@@ -153,6 +186,7 @@ parser.add_argument('-M', '--max', type=int, default=-1, help="max of frames for
 parser.add_argument('-perc', '--percentage', type=float, default=0.25, help="percentage of video to use for background modeling")
 parser.add_argument('-a', '--alpha', metavar='N', nargs='+', type=float, default=11, help="alpha value")
 parser.add_argument('-p', '--p', type=float, default=0.001, help="[AdaptiveGaussianModel] parameter controlling the inclusion of new information to model")
+parser.add_argument('-d', '--display', action='store_true', help="Display frames as they are processed or not")
 args = parser.parse_args()
 
 main(args)
--- week2/utils.py
+++ week2/utils.py
@@ -7,6 +7,53 @@ from sklearn.metrics import average_precision_score
 from matplotlib import pyplot as plt
 
 
+# Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+	# if the bounding boxes integers, convert them to floats --
+	# this is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+	# initialize the list of picked indexes	
+	pick = []
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:,0]
+	y1 = boxes[:,1]
+	x2 = boxes[:,2]
+	y2 = boxes[:,3]
+	# compute the area of the bounding boxes and sort the bounding
+	# boxes by the bottom-right y-coordinate of the bounding box
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = np.argsort(y2)
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+		# find the largest (x, y) coordinates for the start of
+		# the bounding box and the smallest (x, y) coordinates
+		# for the end of the bounding box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+		# delete all indexes from the index list that have
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return pick #boxes[pick].astype("int")
+
 
 def parse_aicity_rects(path):
     """
@@ -34,7 +81,7 @@ def parse_aicity_rects(path):
     return ret_dict
 
 
-def parse_xml_rects(path):
+def parse_xml_rects(path, remove_static=False):
     """
     Input:
         - Path to annotation xml in Pascal VOC format
@@ -49,6 +96,12 @@ def parse_xml_rects(path):
             for x in child:
                 d = x.attrib
                 frame = f"f_{d['frame']}"
+
+                if x[0].text == 'true' and remove_static: #
+                    if frame not in frame_dict:
+                        frame_dict[frame] = []
+                    continue
+
                 if frame not in frame_dict:
                     frame_dict[frame] = []
                 frame_dict[frame].append([float(d['xtl']), float(d['ytl']), 
