ff9f3d6d7f42a5efaec7abeae7540c59ea2fe79b
Carmen GarciaNo <carmengnogu@gmail.com>
2021-03-23 19:01:43 +0100

    Save rects in csv added to utils

--- week3/utils.py
+++ week3/utils.py
@@ -83,9 +83,24 @@ def parse_aicity_rects(path):
 
     return ret_dict
 
-def save_aicity_rects(path, det_rects)
+def save_aicity_rects(path, det_rects):
 
-   COL_NAMES = ['frame', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf', 'x', 'y', 'z']
+    COL_NAMES = ['frame', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf', 'x', 'y', 'z']
+    dic_csv = {
+        'frame' = [], 'id'= [], 'bb_left'= [], 'bb_top'= [], 'bb_width'= [], 'bb_height'= [], 'conf'= [], 'x'= [], 'y'= [], 'z'= []
+    }
+    for f in det_rects:
+        for det in det_rects[f'f_{f}']:
+            dic_csv['frame'].append(f)
+            dic_csv['id'].append(det['id'])
+            dic_csv['bb_left'].append(det['bbox'][0])
+            dic_csv['bb_top'].append(det['bbox'][1])
+            dic_csv['bb_width'].append(det['bbox'][2]-det['bbox'][0])
+            dic_csv['bb_height'].append(det['bbox'][3]-det['bbox'][1])
+            dic_csv['conf'].append(det['conf'])
+            dic_csv['x'].append(-1)
+            dic_csv['y'].append(-1)
+            dic_csv['z'].append(-1)
 
     df = pd.DataFrame(det_rects, columns=COL_NAMES)
     df.to_csv (path, index = False, header=True)
