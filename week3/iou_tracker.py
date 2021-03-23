69090d917c4927bf4af948476af8fd319877387b
Carmen GarciaNo <carmengnogu@gmail.com>
2021-03-23 17:37:33 +0100

    wip

--- week3/iou_tracker.py
+++ week3/iou_tracker.py
@@ -1,5 +1,5 @@
 import utils
-from munkres import Munkres
+# from munkres import Munkres
 
 
 # 2141 frames in total
@@ -16,45 +16,42 @@ TRACKS_COUNTER = 0
 
 class tracked_object:
 
-    def __init__(self, idd, bbox, tracker_life = 5, min_iou_th = 0.5):
+    def __init__(self, idd, bbox, tracker_life = 5):
         self.id = idd
         self.bbox = bbox
         self.tracker_life = tracker_life
-        self.min_iou_th = min_iou_th
         _add_track(self)
 
     def _add_track(self):
-        tracked_object_list.append(self)
+        tracked_object_dic[idd] = self
         return
 
 
 def adj_track(det_bbox):
-        # to access to iou_matrix: iou_matrix[detector_index][tracker_index]
-            best_iou = 0
-            idx = -1
+      
+    best_iou = 0
+    idx = -1
 
-            for id_t in tracked_object_dic:
-                to = tracked_object_dic[id_t]
-                iou = get_rect_iou(to.bbox, det_bbox)
-                if iou > best_iou:
-                    best_iou = iou
-                    idx = track_obj.id
-                
-            if idx != -1 and iou > 0.5:
-                 track_obj.tracker_life  = 5
+    for id_t in tracked_object_dic:
 
-            else:
-                track_obj(bbox)
-                tracked_object_dic[tracked_object_dic]
-                TRACKS_COUNTER+=1
-               
+        iou = get_rect_iou(tracked_object_dic[id_t].bbox, det_bbox)
+        
+        if iou > best_iou:
+            best_iou = iou
+            idx = id_t
+        
+    if int(idx) != -1 and iou > 0.5:
+        tracked_object_dic[idx].tracker_life = 5
+        tracked_object_dic[idx].bbox = bbox
+    else:
+        idx = str(TRACKS_COUNTER)
+        track_obj(idx, bbox)
+        TRACKS_COUNTER+=1
+    return idx
 
-                
-        # iou_matrix = [[1 - get_rect_iou(t_det.bbox, det_bbox) for t_det in tracker_bboxes]
-        #                 for det_bbox in detector_bboxes]
-
-        idxs = Munkres().compute(iou_matrix)
-    return id
+def decrease_memory():
+    for idx in tracked_object_dic:
+        tracked_object_dic[idx].tracker_life -= 1
 
 def main():
 
@@ -65,8 +62,7 @@ def main():
         det_rects[f'f_{f}'] #detections per frame
         for det in det_rects[f'f_{f}']:
             det['id'] = adj_track(det['bbox'])
-
-        print(det_rects[f'f_{f}'])
-    #gt_rects_detformat = {f: [{'bbox': r, 'conf':1} for r in v] for f, v in gt_rects.items()}
-
+            decrease_memory()
+            print(det)
+    
 main()
