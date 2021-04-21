21280c30611ef134a561d0474039dd176b314985
jchavescoelho <jchavescoelho@gmail.com>
2021-04-21 21:34:46 +0200

    WIP adding gifs

--- week5/multi_visualizer.py
+++ week5/multi_visualizer.py
@@ -1,17 +1,26 @@
 import os
 import sys
+import datetime
 import cv2
+import imageio
+import argparse
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+
+from pygifsicle import optimize
+
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
+import utils as w5utils
 from week3 import utils as w3utils
 
-DATA_PATH = '/home/jchaves/code/temp/m6data/'
+DATA_PATH = '/home/capiguri/code/datasets/m6data'
 FRAME_NUM_PATH = os.path.join(DATA_PATH, 'cam_framenum')
 TIMESTAMP_PATH = os.path.join(DATA_PATH, 'cam_timestamp')
+
+GIF_OUT_DIR = './w6gifs/'
 
 dets2use = ['mask_rcnn']
 tracks2use = ['mtsc_deepsort_mask_rcnn']
@@ -37,6 +46,8 @@ class MOTCamera():
         self.det = det
         self.track = track
 
+        self.last_frame = None
+
     def init_capture(self):
         print(f'\tInitializing capture for camera {self.cam_name}...')
         self.frame_cont = 0
@@ -47,6 +58,7 @@ class MOTCamera():
         self.frame_cont += 1
         ret, frame =  self.cap.read()
         frame = self._paint_rects(frame)
+        self.last_frame = frame
         return ret, frame
 
     def _paint_rects(self, im):
@@ -54,8 +66,11 @@ class MOTCamera():
         # det = self.trackings[self.track]
         det = self.gt
         frame = w3utils.pretty_rects(im, det.get(f'f_{self.frame_cont}', []), 'test', (0,255,0),
-                conf_thresh=0.7, tracking = det.get('tracking', False))
+                conf_thresh=0.7, tracking = True)
         return im
+    
+    def get_last_frame(self):
+        return self.last_frame
 
     def _close_vid(self):
         self.cap.release()
@@ -74,13 +89,18 @@ class MOTSequence():
 
         print(f'Creating object for sequence S{str(seq_num).zfill(2)}...')
         path = os.path.join(DATA_PATH, 'train', f'S{str(seq_num).zfill(2)}')
-        self.cams= {p: MOTCamera(os.path.join(path, p), det=det, track=track) for p in os.listdir(path) if int(p[1:]) in cam_ids}
+        self.cams = {p: MOTCamera(os.path.join(path, p), det=det, track=track) for p in os.listdir(path) if int(p[1:]) in cam_ids}
         self.num_cams = len(self.cams)
         self.timestamps = {}
         with open(os.path.join(TIMESTAMP_PATH, f'S{str(seq_num).zfill(2)}.txt'), 'r') as f:
             for l in f:
                 k, v = l.split()
                 self.timestamps[k] = float(v)
+        
+        self.gif_buffer = {k: [] for k in self.cams}
+        self.gifs_to_save = {k: {} for k in self.cams}
+        self.gif_flag = False
+        self.dtstring = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
 
     def init_visualize(self, ids=None):
         self.frame_cont = 0
@@ -91,7 +111,7 @@ class MOTSequence():
                 continue
             cam.init_capture()
     
-    def next_frame(self, ids=None):
+    def next_frame(self, ids=None, txt_info='', scale=0.5):
 
         frames = {}
 
@@ -105,23 +125,74 @@ class MOTSequence():
                 frames[c] = frame if ret else np.zeros((100,100), dtype=np.uint8)
 
         self.frame_cont += 1
-        return self._show_frames(frames)
 
-    def _show_frames(self, frames):
+        if self.gif_flag:
+            for k, v in self.frames:
+                gif_buffer[k].append(v)
+
+        self._show_frames(frames, txt_info=txt_info, scale=scale)
+
+    def _show_frames(self, frames, txt_info='', scale=0.5):
         for k, v in frames.items():
-            v = cv2.resize(v, tuple(np.int0(0.5*np.array(v.shape[:2][::-1]))))
+            v = cv2.resize(v, tuple(np.int0(scale*np.array(v.shape[:2][::-1]))))
+
+            h, w = v.shape[:2]
+            x, y = int(w*0.75), int(h*0.8)
+            txt_info += '[REC]' if self.gif_flag else '[___]'
+            v = cv2.putText(v, txt_info, (x, y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 
+                w3utils.get_optimal_font_scale(txt_info, w*0.1), (0, 0, 255), 2)
+
             cv2.imshow(k, v)
-        return cv2.waitKey(0)
+        # return cv2.waitKey(0)
     
     def __del__(self):
         for _,c in self.cams.items():
             del c
+    
+    def toggle_gifs(self):
+        if self.gif_flag:
+            for k, v in self.cams:
+                self.gif_flag = False
+                self.gifs_to_save[k][os.path.join(GIF_OUT_DIR, self.dtstring, f'gif_{v.cam_name}_{v.frame_cont}.gif')] = gif_buffer
+                self.gif_buffer[k] = None
+        else:
+            print('Init gif')
+            self.gif_flag = True
+            for k, v in self.cams.items():
+                self.gif_buffer[k] = [w5utils.gif_preprocess(v.get_last_frame())]
 
 
-s = MOTSequence(3, cam_ids=[10, 11, 12, 13, 14, 15])
+os.makedirs(GIF_OUT_DIR, exist_ok=True)
+
+s = MOTSequence(3, cam_ids=[13, 14, 15])
 s.init_visualize()
+
+WAIT_TIME_LIST = [30, 60, 0, 10] # Available display speeds
+wait_time_idx = 0 # start at 33 FPS (15 ms per frame)
+disp_scale = 0.5
 
 k = True
 while k != ord('q'):
-    k = s.next_frame()
+    wait_time = WAIT_TIME_LIST[wait_time_idx % len(WAIT_TIME_LIST)]
+    FPS = int(1e3/wait_time if wait_time != 0 else 0)
 
+    s.next_frame(txt_info=f'{FPS} FPS', scale=disp_scale)
+
+    # Keyboard controls
+    k = cv2.waitKey(wait_time)
+
+    if k == ord('q'): # quit
+        break
+    elif k == ord('+'):
+        disp_scale += 0.1 if disp_scale < 1 else 0
+    elif k == ord('-'):
+        disp_scale -= 0.1 if disp_scale > 0.1 else 0
+    # elif k == ord('s'): # Snapshot
+    #     cv2.imwrite(f'save_{frame_cont}.png', display_frame)
+    elif k == ord('g'):  # toggle gif
+        s.toggle_gifs()
+    elif k == ord('p'): # Pause
+        wait_time_idx += 1
+        # wait_time = int(not(bool(wait_time)))
+    
+print(s.gifs_to_save)
--- week5/utils.py
+++ week5/utils.py
@@ -15,3 +15,8 @@ def get_VIDEO_path(sequence, camera):
 def get_DET_path(sequence, camera, algorithm):
     # sequence: sequence ID integer, camera: camera ID integer, algorithm:={yolo3, ssd512, mask_rcnn}
     return f"../data/aic19-track1-mtmc-train/train/S{sequence:02d}/c{camera:03d}/det/det_{algorithm}.txt"
+
+def gif_preprocess(im, width=512):
+    im = utils.resize_keep_ap(im, width=width)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    return im
